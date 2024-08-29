b5649462994596549b3c7ab691df5280006f849f
Atheo Jester <atheojetster@gmail.com>
2024-08-29 12:51:01 +0800

    Change video, add animation, change design coach

--- Product Landing Page/animate.js
+++ Product Landing Page/animate.js
@@ -3,27 +3,47 @@ const observer = new IntersectionObserver((entries) => {
     console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
+
+      const video = document.getElementById("video");
+      const videoSrc = video.src;
+      if (!videoSrc.includes("autoplay=1")) {
+        video.src = videoSrc + "&autoplay=1"; // Append autoplay=1 to the existing src
+      }
+
+      observer.unobserve(entry.target);
     } else {
       entry.target.classList.remove("show");
     }
   });
 });
 
+const observerX = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showX");
+    } else {
+      entry.target.classList.remove("showX");
+    }
+  });
+});
+
+const video = document.getElementById("video");
+
 const hiddenElements = document.querySelectorAll(".hidden");
 
+const hiddenElementsX = document.querySelectorAll(".hiddenX");
+
+const vidContainer = document.querySelector(".vidContainer");
+
+const aboutElement = document.querySelectorAll(".about");
+
 hiddenElements.forEach((el) => observer.observe(el));
+if (vidContainer) {
+  observer.observe(vidContainer);
+}
 
-// const upObserver = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("upShow");
-//     } else {
-//       entry.target.classList.remove("upShow");
-//     }
-//   });
-// });
-
-// const hiddenUpward = document.querySelectorAll(".hiddenUp");
-
-// hiddenUpward.forEach((el) = observer.observe(el));
+hiddenElementsX.forEach((el) => observerX.observe(el));
+if (aboutElement) {
+  observerX.observe(aboutElement);
+}
