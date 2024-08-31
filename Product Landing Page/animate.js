c9d901c8864a98fe0119d219c3fa94a2ba66bae9
Atheo Jester <atheojetster@gmail.com>
2024-08-31 11:50:38 +0800

    Test Revert

---
+++ Product Landing Page/animate.js
@@ -0,0 +1,64 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+
+      const video = document.getElementById("video");
+      const videoSrc = video.src;
+      if (!videoSrc.includes("autoplay=1")) {
+        video.src = videoSrc + "&autoplay=1"; // Append autoplay=1 to the existing src
+      }
+
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const observerY = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const observerX = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showX");
+    } else {
+      entry.target.classList.remove("showX");
+    }
+  });
+});
+
+const video = document.getElementById("video");
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const hiddenElementsX = document.querySelectorAll(".hiddenX");
+
+const vidContainer = document.querySelector(".vidContainer");
+
+const aboutElement = document.querySelectorAll(".about");
+
+const hiddenElementsY = document.querySelectorAll(".hiddenY");
+
+hiddenElements.forEach((el) => observer.observe(el));
+if (vidContainer || showCoach) {
+  observer.observe(vidContainer);
+}
+
+hiddenElementsY.forEach((el) => observerY.observe(el));
+
+hiddenElementsX.forEach((el) => observerX.observe(el));
+if (aboutElement) {
+  observerX.observe(aboutElement);
+}
